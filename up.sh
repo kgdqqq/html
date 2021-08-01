5bdaf5434b963336437fd20105db51cec1705521
kgdqqq <winpe@msn.cn>
2021-08-01 14:14:11 +0800

    Create up.sh

---
+++ up.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#关闭内核机制
+#sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux && setenforce 0
+if [[ $linux_release =~ $release_centos6 ]];then
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
+elif [[ $linux_release =~ $release_centos7 ]];then
+sed -i "s#SELINUX=enforcing#SELINUX=disabled#g" /etc/selinux/config
+fi
+
+#获取当前系统的发行版本
+VERSION=$(cat /etc/centos-release)
+
+#提取当前系统的版本号
+V_NUM=${VERSION:21:1}
+
+BASE_REPO="/etc/yum.repos.d/CentOS-Base.repo"
+ALI_REPO="http://mirrors.aliyun.com/repo/Centos-${V_NUM}.repo"
+
+echo "备份当前软件源..."
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+echo "备份完成: /etc/yum.repos.d/CentOS-Base.repo.backup"
+echo "下载阿里云镜像源..."
+wget -O ${BASE_REPO} ${ALI_REPO} || curl -o ${BASE_REPO} ${ALI_REPO}
+#补丁程序, 防止出现 Couldn't resolve host 'mirrors.cloud.aliyuncs.com' 信息
+sed -i -e '/mirrors.cloud.aliyuncs.com/d' -e '/mirrors.aliyuncs.com/d' /etc/yum.repos.d/CentOS-Base.repo
+echo "清除缓存..."
+yum clean all
+echo "缓存清除成功,OK"
+
+echo "生成缓存..."
+yum makecache
+echo "生成缓存成功, OK"
+
+echo "更新软件..."
+yum update -y
+echo "软件更新完毕, OK"
