6a76f5c49b0ba5d8fdbd8479a2ecbc9452cc09ab
kgdqqq <winpe@msn.cn>
2021-08-01 14:03:45 +0800

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,4 @@
+sudo sed -e 's|^mirrorlist=|#mirrorlist=|g' \
+         -e 's|^#baseurl=http://mirror.centos.org|baseurl=https://mirrors.tuna.tsinghua.edu.cn|g' \
+         -i.bak \
+         /etc/yum.repos.d/CentOS-*.repo
