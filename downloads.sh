d51ed955e54b0113ebfe546af257b493c95661da
kgdqqq <winpe@msn.cn>
2021-09-02 12:30:58 +0800

    Create downloads.sh

---
+++ downloads.sh
@@ -0,0 +1,32 @@
+mkdir /home/aria2
+mkdir /home/downloads
+mkdir /home/aria2/aria2-config
+mkdir /home/aria2/script
+
+
+wget -O /home/aria2/aria2-config/clean.sh https://kgdqqq.github.io/Other/aria2/clean.sh
+
+wget -O /home/aria2/aria2-config/aria2.session https://kgdqqq.github.io/Other/aria2/aria2.session
+
+wget -O /home/aria2/aria2-config/dht.dat https://kgdqqq.github.io/Other/aria2/dht.dat
+
+wget -O /home/aria2/aria2-config/dht6.dat https://kgdqqq.github.io/Other/aria2/dht6.dat
+
+wget -O /home/aria2/aria2-config/script/delete.sh https://kgdqqq.github.io/Other/aria2/delete.sh
+
+wget -O /home/aria2/aria2-config/aria2.conf https://kgdqqq.github.io/Other/aria2/aria2.conf
+
+wget -O /home/downloads.yml https://kgdqqq.github.io/Other/aria2/downloads.yml
+
+docker-compose -f /home/downloads.yml up -d
+
+
+
+mkdir /home/qbittorrent
+
+mkdir /home/qbittorrent/config
+
+wget -O /home/qbittorrent/qbittorrent.yml https://kgdqqq.github.io/Other/qbittorrent/qbittorrent.yml
+
+docker-compose -f /home/qbittorrent.yml up -d
+
