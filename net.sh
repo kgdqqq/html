b0012ab6390a52b9432c2434c23a7f9d42ba956e
端木 <80463034+kgdqqq@users.noreply.github.com>
2021-10-21 17:42:33 +0800

    Create net.sh

---
+++ net.sh
@@ -0,0 +1,6 @@
+systemctl stop NetworkManager && systemctl disable NetworkManager
+
+systemctl stop NetworkManager && systemctl disable NetworkManager
+yum install network-scripts -y    
+systemctl start network && systemctl enable network
+systemctl start network && systemctl enable network
